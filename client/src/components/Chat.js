f045c0ceeb82773998d657f2d55f6ad0a323c4cf
MathiasMantai <mmantaibusiness@gmail.com>
2022-07-25 15:34:32 +0200

    chat and message styling

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -18,22 +18,23 @@ export default class Chat extends React.Component {
                 message: this.state.message,
                 time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
             }
-
-
             //eigene nachrichten andere farbe als fremde
             //eigene nachrichtren nach rechts schieben
-            
-            this.setState({messageList: [this.state.messageList, <Message author={messageData.author} message={messageData.message} time={messageData.time} />]});
-
-            console.log(this.state.messageList);
+            this.setState(
+                {messageList: [this.state.messageList, <div className="w-full"><Message author={messageData.author} message={messageData.message} time={messageData.time} source="self" /></div>]}
+            );
+            // console.log(this.state.messageList);
             await this.props.socket.emit("sendMessage", messageData);
+            this.setState({message: ""});
         }
     }
 
     componentDidMount = () => {
         this.props.socket.on("receiveMessage", (data) => {
-            this.setState({messageList: [this.state.messageList, <Message author={data.author} message={data.message } time={data.time} />]});
-            console.log(this.state.messageList);
+            this.setState(
+                {messageList: [this.state.messageList, <div className="w-full"><Message author={data.author} message={data.message } time={data.time} source="other" /></div>]}
+            );
+            // console.log(this.state.messageList);
         });
     }
 
@@ -44,26 +45,26 @@ export default class Chat extends React.Component {
 
     disconnect = () => {
         this.props.setShowChat(false);
+        this.props.socket.emit("disconnectRoom", this.props.room);
     }
 
     render() {
         return (
-            <div>
+            <div className="w-1/2 content-box border border-md border-black block">
                 <div id="chat-header">
                     {this.props.room} <button onClick={this.disconnect}>Verbindung Trennen</button>
                 </div>
-                <div id="chat-body">
+                <div id="chat-body h-1/2 min-h-1/2 max-h-1/2 overflow-auto block p-2">
                     {this.state.messageList}
                 </div>
                 <div id="chat-footer">
-                    <input type="text" placeholder="..." onChange={(event) => {
+                    <input type="text" placeholder="..." value={this.state.message} onChange={(event) => {
                         this.setState({message: event.target.value});
                     }}
                       />
-                    <button onClick={this.sendMessage}>Senden</button>
+                    <button className=""onClick={this.sendMessage}>Senden</button>
                 </div>
             </div>
-
         )
     }
 }
--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,16 +1,18 @@
-const Message = ({author, message, time}) => {
+const Message = ({author, message, time, source}) => {
     return (
-        <div className="">
-            <div>
+        <div className={" grid mb-6 h-1/2 min-h-1/2 max-h-1/2 overflow-auto break-words " + (source === "self" ? "place-items-start" : "place-items-end") }>
+            <div className="">
+            <div className={"text-white text-left p-2 border border-md rounded-md " + (source === "self" ? "bg-emerald-600" : "bg-sky-600")}>
                 {message}
             </div>
-            <div className="flex flex-row content-between">
-                <div>
+            <div className="flow-root text-sm">
+                <div className="float-left mr-1">
                     {time}
                 </div>
-                <div>
+                <div className="float-right ml-1">
                     {author}
                 </div>
+            </div>
             </div>
         </div>
     )
